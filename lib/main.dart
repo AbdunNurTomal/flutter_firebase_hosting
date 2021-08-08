c7960a0819e239b2590c947870695f4ea6160dea
Abdun Nur Tomal <tomal.nur@gmail.com>
2021-08-08 17:50:09 +0600

    Add all my files

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_hosting/page/newsletter_page.dart';
+
+Future main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Abdun Nur Tomal';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primaryColor: Color(0xFFed2229),
+        ),
+        home: NewsletterPage(),
+      );
+}
